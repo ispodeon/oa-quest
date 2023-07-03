a9c4f6b0c69a2e0ec61e61ab38555119e2f16b17
Ryan Boyd <ispodeon@yahoo.com>
2023-07-03 05:36:30 -0400

    completed task 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -11,6 +12,7 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
     ])
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,10 +10,38 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
+  public name: string = '';
+  public count: number = 0;
+  public items: TodoItem[] = [];
+
+  addItem(itemname: string){
+    this.items.push({id: this.count++, name: itemname, isComplete: false});
+  }
+
+  complete(index: number){
+    this.items[index].isComplete = this.items[index].isComplete ? false : true;
+
+    console.log("index",index);
+    
+    console.log(this.items[index].isComplete);
+    
+  }
+
+  displayStatus(){
+    let total = 0;
+    for (let index = 0; index < this.items.length; index++) {
+      total += this.items[index].isComplete ? 0: 1;
+      
+    }
+    return `${total} remaining out of ${this.items.length} tasks`;
+  }
+
   share() {
     window.alert('The product has been shared!');
   }
 }
+
+export interface TodoItem { id: number; name: string; isComplete: boolean}
 
 
 /*
